f538c7fe575185ea7997bbf83a3e65ac2b3c11b3
Caio Santos <50676424+xCaio@users.noreply.github.com>
2022-02-19 12:58:52 -0300

    Update televisao.dart

--- televisao.dart
+++ televisao.dart
@@ -1,33 +1,47 @@
-void main(){
-
-  Televisao televisao = Televisao();
-  televisao.LigaDesliga();
-  
-  print(televisao.status);
-  print(televisao.canal);
-  print(televisao.volume);
-  
-}
-
 class Televisao{
-  bool? status;
-  int? canal;
-  int? volume;
-
-  Televisao(){
-    this.status=false;
-  }
-
+  bool status = false;
+  int canal = 0;
+  int volume = 0;
   void LigaDesliga(){
     if(this.status == false){
-      this.status = true;
-      this.canal =3;
-      this.volume=10;
-      print("LIGADA");
-
+      print("[ LIGADA ]");
+      this.canal = 3;
+      this.volume = 10;
+      this.status == true;
     }else{
-      this.status=false;
-      print("DESLIGADA");
+      print("DESLIGANDO...");
+      this.status = false;
     }
+
   }
+  AumentaCanal(){
+    this.canal = canal + 1;
+  }
+
+  AumentaVolume(){
+    this.volume++;
+  }
+
+  int MostraCanal(){
+    print("CANAL: ");
+    return this.canal;
+  }
+  int MostraVolume(){
+    print("VOLUME: ");
+    return this.volume;
+  }
+}
+void main(){
+  Televisao televisao = Televisao();
+  televisao.LigaDesliga();
+  televisao.MostraCanal();
+  print(televisao.canal);
+  televisao.MostraVolume();
+  print(televisao.volume);
+  print("AUMENTANDO CANAL");
+  televisao.AumentaCanal();
+  print(televisao.canal);
+  print("AUMENTANDO VOLUME");
+  televisao.AumentaVolume();
+  print(televisao.volume);
 }
